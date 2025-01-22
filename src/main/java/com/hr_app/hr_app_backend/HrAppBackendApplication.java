e0e10f3dffd176cdce306afafecf6d9ecd58d23d
bintangmada <bintang.mada.suharsono@bni.co.id>
2025-01-22 19:41:36 +0700

    add spring boot base project

---
+++ src/main/java/com/hr_app/hr_app_backend/HrAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hr_app.hr_app_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrAppBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrAppBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hr_app/hr_app_backend/HrAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr_app.hr_app_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
