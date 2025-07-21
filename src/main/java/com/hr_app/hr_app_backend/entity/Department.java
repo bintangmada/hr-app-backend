715328af120a156bff3945510fe2a715591172a8
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2025-07-21 16:26:20 +0700

    update department entity

--- src/main/java/com/hr_app/hr_app_backend/entity/Department.java
+++ src/main/java/com/hr_app/hr_app_backend/entity/Department.java
@@ -15,13 +15,51 @@ public class Department {
     @Column(name = "department_name", nullable = false, length = 50)
     private String departmentName;
 
-    // RELATION TO LOCATION
-    @ManyToOne
-    @JoinColumn(name = "location_id", nullable = false)
-    private Location location;
+    @Column(name = "manager_id")
+    private Long managerId;
 
-    @OneToMany(mappedBy = "department")
-    private List<Employee> employee;
+    @Column(name = "locationId")
+    private Long locationId;
 
+    public Department(Long departmentId, String departmentName, Long managerId, Long locationId) {
+        this.departmentId = departmentId;
+        this.departmentName = departmentName;
+        this.managerId = managerId;
+        this.locationId = locationId;
+    }
 
+    public Department() {
+    }
+
+    public Long getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Long departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
+
+    public Long getManagerId() {
+        return managerId;
+    }
+
+    public void setManagerId(Long managerId) {
+        this.managerId = managerId;
+    }
+
+    public Long getLocationId() {
+        return locationId;
+    }
+
+    public void setLocationId(Long locationId) {
+        this.locationId = locationId;
+    }
 }
